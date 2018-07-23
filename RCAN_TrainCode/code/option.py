5a1d981915623d44aff473bcb0588013968da11c
Yulun Zhang <yulun100@gmail.com>
2018-07-23 13:23:57 -0700

    --epoch 1000

--- RCAN_TrainCode/code/option.py
+++ RCAN_TrainCode/code/option.py
@@ -77,7 +77,7 @@ parser.add_argument('--reset', action='store_true',
                     help='reset the training')
 parser.add_argument('--test_every', type=int, default=1000,
                     help='do test per every N batches')
-parser.add_argument('--epochs', type=int, default=3000,
+parser.add_argument('--epochs', type=int, default=1000,
                     help='number of epochs to train')
 parser.add_argument('--batch_size', type=int, default=16,
                     help='input batch size for training')
